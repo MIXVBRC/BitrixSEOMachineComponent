a47dd4336982fad78071fdc4fa16478fc95ea01a
Max Fedorov <mixvbrc@gmail.com>
2021-03-15 03:26:19 +1000

    Start

---
+++ .description.php
@@ -0,0 +1,18 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = [
+	"NAME" => "SEO Machine",
+	"DESCRIPTION" => "SEO оптимизация сайта",
+    "SORT" => 1000,
+    "CACHE_PATH" => "Y",
+	"COMPLEX" => "N",
+	"PATH" => [
+		"ID" => "mixvbrc",
+		"NAME" => "MIXVBRC"
+	],
+    "AREA_BUTTONS" => [
+        [
+            'URL' => "https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA%D0%BE%D0%B2%D0%B0%D1%8F_%D0%BE%D0%BF%D1%82%D0%B8%D0%BC%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F",
+            'TITLE' => "Что такое SEO ?"
+        ],
+    ],
+];
